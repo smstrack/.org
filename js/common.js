8dcd780623ae9e6d9a72153fda970be81e4424e8
smsraidertrack <coachcjohnson@gmail.com>
2017-02-19 22:57:40 -0500

    common headers/footers

--- js/common.js
+++ js/common.js
@@ -11,9 +11,9 @@ function displayHeader()
 	document.write("<a target='_blank' href='http://www.youtube.com/user/smsraidertrack'>");
 	document.write("<img src='/images/youtube-icon-full_color_32.png' alt='Youtube channel' class='style22' align='middle'></a>");
 	document.write("<br>");
-	document.write("<a href='http://www.smstrack.org'><img src='/images/clear.gif' alt='' style=' border:0;'></a>");
+	document.write("<a href='/index.htm'><img src='/images/clear.gif' alt='' style=' border:0;'></a>");
 	document.write("</td>");
-	document.write("<td class='style3' onclick='location.href='http://www.smstrack.org'' style='cursor:pointer;'>");
+	document.write("<td class='style3' onclick='location.href=&quot;/index.htm&quot;' style='cursor:pointer;'>");
 	document.write("<img border='0' src='/images/run.png' alt=''></td>");
 	document.write("</tr>");
 
@@ -80,4 +80,70 @@ function displayFooter()
 	document.write("</p>");
 	document.write("</td>");
 	document.write("</tr>");
+}
+
+function displayTeamHeader()
+{
+	document.write("<div class='menubar2'>");
+	document.write("<table align='center' width='100%' class='menubar2'>");
+	document.write("<tr class='menuba2r'>");
+	document.write("<td><a href='guidelines.htm'>Policies</a></td>");
+	document.write("<td><a href='rules.htm'>Rules</a></td>");
+	document.write("<td><a href='forms.htm'>Forms</a></td>");
+	document.write("<td><a href='roster.htm'>Roster</a></td>");
+	document.write("<td><a href='throws/index.htm'>Shot/Disc</a></td>");
+	document.write("<td><a href='https://store.thegraphicedge.com/eFlyer/main.asp?stid=southtrackfield17' target='_blank'>SMSTF Team Gear</a></td>");
+	document.write("</tr>");
+	document.write("</table>");
+	document.write("</div>");
+
+}
+
+function displayRecordsHeader1()
+{
+	document.write("<div class='menubar2'>");
+	document.write("<table align='center' width='100%' class='menubar2'>");
+	document.write("<tr class='menuba2r'>");
+	document.write("<td><a href='/archives/toptenalltimeboys.htm'>Records/Top Ten</a></td>");
+	document.write("<td><a href='/archives/state_champion_teams.htm'>Team Championships</a></td>");
+	document.write("<td><a href='/archives/state_champs_by_name.htm'>Individual State Champions</a></td>");
+	document.write("</tr>");
+	document.write("</table>");
+	document.write("</div>");
+}
+
+function displayRecordsHeader2()
+{
+	document.write("<div class='menubar3'>");
+	document.write("<table align='center' width='100%' class='menubar3'>");
+	document.write("<tr class='menubar3'>");
+	document.write("<td><a href='../archives/toptenalltimeboys.htm'>Varsity/Top Ten</a></td>");
+	document.write("<td><a href='../records/boysfreshmanrecords.htm'>Freshman</a></td>");
+	document.write("<td><a href='../records/smsrelaysboys.htm'>South Relays</a></td>");
+	document.write("</tr>");
+	document.write("</table>");
+	document.write("</div>");
+}
+
+function displayThrowsHeader()
+{
+	document.write("<div id='banner' class='style1'>");
+	document.write("<table cellspacing='0' cellpadding='0' class='style99' style='width: 100%; height: 200px;'>");
+	document.write("<tr onclick='location.href=&quot;index.htm&quot;' style='cursor:pointer;'>");
+	document.write("<td align='left' valign='top'>");
+	document.write("</td>");
+	document.write("</tr>");
+	document.write("</table>");
+	document.write("</div>");
+	document.write("<div style='max-width:1597px' id='menubar' class='menubar'>");
+	document.write("<table class='menubar' border='0' cellpadding='0' style='border-collapse: collapse' width='100%' id='table1'>");
+	document.write("<tr class='menubar'>");
+	document.write("<td><a href='/index.htm'>Track Home</a></td>");
+	document.write("<td><a href='formchart.htm'>Form Chart</a></td>");
+	document.write("<td><a href='videos.htm'>Videos</a></td>");
+	document.write("<td><a href='links.htm'>Links</a></td>");
+	document.write("<td><a href='/team.htm'>Team</a></td>");
+	document.write("</tr>");
+	document.write("</table>");
+	document.write("</div>");
 }
