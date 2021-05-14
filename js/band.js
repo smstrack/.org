31fd3538a93e0b7d352b2102e62744351f69a5e6
coachcjohnson <coachcjohnson@gmail.com>
2021-05-14 14:18:20 -0500

    band test update

--- js/band.js
+++ js/band.js
@@ -1,9 +1,12 @@
-
+const ACCESS_TOKEN = "ZQAAAX427aneVq70QpIz2bnepEX_pgwI0mioOJck9Pdola4-IPqEQkSkNx-TTmoRw_NSFmLuU6t-Lb0s8dd5eLHTp2dWUrWSPOtnhoB38mJuZ2Jx";
+const SMSTF_KEY = "AABoOkqd1PNn4cndslgrrYNt";
+const JJW = "AACfd-hwMHw1gZPtvazp6GxA";
+const HEADLINE_MAX = 25;
 
 $.get("https://openapi.band.us/v2/band/posts",
 	{
-		access_token: "ZQAAAX427aneVq70QpIz2bnepEX_pgwI0mioOJck9Pdola4-IPqEQkSkNx-TTmoRw_NSFmLuU6t-Lb0s8dd5eLHTp2dWUrWSPOtnhoB38mJuZ2Jx",
-		band_key: "83079051",
+		access_token: ACCESS_TOKEN,
+		band_key: SMSTF_KEY,
 		locale: "en_US"
 	}, bandResponse);
 
@@ -11,18 +14,39 @@ function bandResponse(response) {
 	var data = "";
 
 	var resultCode = response.result_code;
-	if (resultCode == 200) {
+	if (resultCode == 1) {
 		var items = response.result_data.items;
 
 		for (var count = 0; count < items.length; count++) {
-			var stringStrArray = items[count].published.split(/[- T:]/);
 
-			date = new Date(stringStrArray[0], stringStrArray[1] - 1, stringStrArray[2], stringStrArray[3], stringStrArray[4], stringStrArray[5]);
+			var item = items[count];
+			var author = item.author;
+			var content = item.content;
 
-			data += "<h2 class='headline'>" + items[count].title + "</h2>";
-			data += "<div class='byline'>" + date.toLocaleString() + "</div>";
-			data += "<p>" + items[count].content + "</p><hr>";
+
+			if (author.role == "leader" || author.role == "coleader") {
+				date = new Date(item.created_at);
+
+				var headline = content;
+				if (content.length > 25) {
+					var stop = content.indexOf(" ", HEADLINE_MAX);
+					if (stop != -1) {
+						headline = content.slice(0, stop) + "...";
+					}
+				}
+
+				body = linkify(content);
+
+				data += "<h2 class='headline'>" + headline + "</h2>";
+				data += "<div class='byline'><b>" + author.name + " </b><small>" + author.description + "</small> - " + date.toLocaleString() + "</div>";
+				data += "<p>" + body + "</p><hr>";
+			}
 		}
 	}
 	document.getElementById("bandlist").innerHTML = data;
+
+	function linkify(text) {
+		var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
+		return text.replace(exp, "<a href='$1' target='_blank'>$1</a>");
+	}
 }
